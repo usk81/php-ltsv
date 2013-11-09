3ed445b5c7a8e224c2d57972d86e517618e293c7
ne_Sachirou <utakata.c4se@gmail.com>
2013-11-10 01:41:29 +0900

    Fixed: encoding has some escape mistakes.

--- example.php
+++ example.php
@@ -1,11 +1,11 @@
 <?php
 require_once __DIR__ . '/lib/ltsv.php';
 
-$test_string = 'time:28/Feb/2013:12:00:00 +0900\thost:192.168.0.1\treq:GET /list HTTP/1.1\tstatus:200';
+$test_string = "time:28/Feb/2013:12:00:00 +0900\thost:192.168.0.1\treq:GET /list HTTP/1.1\tstatus:200";
 
 $test_array = array(
-	'label1_1:value1_1\tlabel1_2:value1_2',
-	'label2_1:value2_1\tlabel2_2:value2_2'
+	"label1_1:value1_1\tlabel1_2:value1_2",
+	"label2_1:value2_1\tlabel2_2:value2_2"
 );
 
 $test_value =  array(
@@ -19,18 +19,18 @@ $result_string = Ltsv::decode($test_string, array('encoding' => 'utf-8'));
 $result_array  = Ltsv::decode($test_array, array('encoding' => 'utf-8'));
 $result_value  = Ltsv::encode($test_value, array('encoding' => 'utf-8'));
 
-echo 'Decode Test:' . "<br />";
-echo 'String Test:' . "<br />";
+echo 'Decode Test:' . '<br />';
+echo 'String Test:' . '<br />';
 echo '<pre>';
 var_dump($result_string);
 echo '</pre>';
-echo "<br />";
-echo 'Array Test:' . "<br />";
+echo '<br />';
+echo 'Array Test:' . '<br />';
 echo '<pre>';
 var_dump($result_array);
 echo '</pre>';
 
-echo 'Encode Test:' . "<br />";
+echo 'Encode Test:' . '<br />';
 echo '<pre>';
 var_dump($result_value);
 echo '</pre>';
